f42729a4a15f2989c8c8a936dbca7182a32ce430
Ph3nol <ph3@slynett.com>
2012-09-09 23:13:39 +0200

    First Atoum tests

---
+++ tests/units/Sly/NotificationPusher/Pusher/BasePusher.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace tests\units\Sly\NotificationPusher\Pusher;
+
+require_once __DIR__ . '/../../../../../vendor/autoload.php';
+
+use mageekguy\atoum;
+use Sly\NotificationPusher\Pusher\BasePusher as BaseBasePusher;
+use Sly\NotificationPusher\Exception\ConfigurationException;
+use Sly\NotificationPusher\Model\Message;
+
+/**
+ * BasePusher.
+ *
+ * @uses atoum\test
+ * @author Cédric Dugat <ph3@slynett.com>
+ */
+class BasePusher extends atoum\test
+{
+    public function testClass()
+    {
+        $this->testedClass
+            ->hasNoParent()
+            ->hasInterface('Sly\NotificationPusher\Pusher\BasePusherInterface')
+        ;
+    }
+
+    public function testConstructWithoutDevices()
+    {
+        /**
+         * Check no device IDs exception.
+         */
+        $this->assert
+            ->exception(function() {
+                $basePusher = new BaseBasePusher(array());
+            })
+            ->isInstanceOf('Sly\NotificationPusher\Exception\ConfigurationException')
+        ;
+    }
+
+    public function testConstructWithDevices()
+    {
+        $basePusher = new BaseBasePusher(array(
+            'devices' => array('ABC', 'DEF'),
+        ));
+
+        /**
+         * Check device IDs.
+         */
+        $this->assert
+            ->array($basePusher->getDevicesUUIDs())
+            ->hasSize(2)
+            ->containsValues(array('ABC', 'DEF'))
+        ;
+
+        /**
+         * Check messages collection.
+         */
+        $this->assert
+            ->object($basePusher->getMessages())
+            ->isInstanceOf('ArrayIterator')
+        ;
+    }
+
+    public function testDefaultConfig()
+    {
+        $basePusher = new BaseBasePusher(array(
+            'devices' => array('ABC', 'DEF'),
+        ));
+
+        $basePusherConfig = $basePusher->getConfig();
+
+        /**
+         * Check some config keys.
+         */
+        $this->assert
+            ->array($basePusherConfig)
+            ->hasKeys(array('dev', 'simulate'))
+        ;
+
+        /**
+         * Check if 'dev' parameter is false by default.
+         */
+        $this->assert
+            ->boolean($basePusherConfig['dev'])
+            ->isFalse()
+        ;
+
+        /**
+         * Check if 'simulate' parameter is false by default.
+         */
+        $this->assert
+            ->boolean($basePusherConfig['simulate'])
+            ->isFalse()
+        ;
+    }
+
+    public function testAddMessage()
+    {
+        $basePusher = new BaseBasePusher(array(
+            'devices' => array('ABC', 'DEF'),
+        ));
+
+        /**
+         * Test with 1, 2 and 3 messages.
+         */
+        for ($i = 1; $i < 3; $i++) {
+            $message = new Message(sprintf('Test %d', $i));
+            $basePusher->addMessage($message);
+
+            $this->assert
+                ->object($basePusher->getMessages())
+                ->isInstanceOf('ArrayIterator')
+                ->hasSize($i)
+            ;
+        }
+    }
+}
