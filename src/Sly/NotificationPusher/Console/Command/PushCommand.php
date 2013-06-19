29fcd61cbfe1f61563b38ec51dd58c08b8ff046f
Cédric Dugat <c.dugat@highco.fr>
2013-06-19 12:07:47 +0200

    PushCommand update

--- src/Sly/NotificationPusher/Console/Command/PushCommand.php
+++ src/Sly/NotificationPusher/Console/Command/PushCommand.php
@@ -2,9 +2,21 @@
 
 namespace Sly\NotificationPusher\Console\Command;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\Output\OutputInterface
+;
+
+use Sly\NotificationPusher\PushManager,
+    Sly\NotificationPusher\Model\Device,
+    Sly\NotificationPusher\Model\Message,
+    Sly\NotificationPusher\Model\Push,
+    Sly\NotificationPusher\Exception\AdapterException
+;
+
+use Doctrine\Common\Util\Inflector;
 
 /**
  * PushCommand.
@@ -23,7 +35,45 @@ class PushCommand extends Command
 
         $this
             ->setName('push')
-            ->setDescription('Manual notification push');
+            ->setDescription('Manual notification push')
+            ->addArgument(
+                'adapter',
+                InputArgument::REQUIRED,
+                'Adapter (apns, gcm, specific class name, ...)'
+            )
+            ->addArgument(
+                'token',
+                InputArgument::REQUIRED,
+                'Device Token or Registration ID'
+            )
+            ->addArgument(
+                'message',
+                InputArgument::REQUIRED,
+                'Message'
+            )
+            ->addOption(
+               'certificate',
+               null,
+               InputOption::VALUE_OPTIONAL,
+               'Certificate path (for APNS adapter)'
+            )
+            ->addOption(
+               'api-key',
+               null,
+               InputOption::VALUE_OPTIONAL,
+               'API key (for GCM adapter)'
+            )
+            ->addOption(
+               'env',
+               PushManager::ENVIRONMENT_DEV,
+               InputOption::VALUE_OPTIONAL,
+               sprintf(
+                    'Environment (%s, %s)',
+                    PushManager::ENVIRONMENT_DEV,
+                    PushManager::ENVIRONMENT_PROD
+                )
+            )
+        ;
     }
 
     /**
@@ -31,5 +81,74 @@ class PushCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $adapter     = $this->getReadyAdapter($input, $output);
+        $pushManager = new PushManager($input->getOption('env'));
+        $message     = new Message('This is an example.');
+        $push        = new Push($adapter, new Device($input->getArgument('token')), $message);
+        $pushManager->add($push);
+
+        $pushes = $pushManager->push();
+
+        var_dump($pushes);
+    }
+
+    /**
+     * Get adapter class from argument.
+     *
+     * @param string $argument Given argument
+     */
+    private function getAdapterClassFromArgument($argument)
+    {
+        if (
+            !class_exists($adapterClass = $argument) &&
+            !class_exists($adapterClass = '\\Sly\\NotificationPusher\\Adapter\\'.ucfirst($argument))
+        ) {
+            throw new AdapterException(
+                sprintf(
+                    'Adapter class %s does not exist',
+                    $adapterClass
+                )
+            );
+        }
+
+        return $adapterClass;
+    }
+
+    /**
+     * @see Command
+     *
+     * @return \Sly\NotificationPusher\Adapter\AdapterInterface
+     */
+    private function getReadyAdapter(InputInterface $input, OutputInterface $output)
+    {
+        $adapterClass = $this->getAdapterClassFromArgument($input->getArgument('adapter'));
+
+        try {
+            $adapter = new $adapterClass();
+        } catch (\Exception $e) {
+            $adapterData = array();
+            preg_match_all('/"(.*)"/i', $e->getMessage(), $matches);
+
+            foreach ($matches[1] as $match) {
+                $optionKey = str_replace('_', '-', Inflector::tableize($match));
+                $option    = $input->getOption($optionKey);
+
+                if (!$option) {
+                    throw new AdapterException(
+                        sprintf(
+                            'The option "%s" is needed by %s adapter',
+                            $optionKey,
+                            $adapterClass
+                        )
+                    );
+                }
+
+                $adapterData[$match] = $option;
+            }
+
+            $adapter = new $adapterClass($adapterData);
+        }
+
+        return $adapter;
     }
 }
