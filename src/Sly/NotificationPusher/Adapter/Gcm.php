0132c9b2145083008323434e3a51dbccf05021fa
Josh Hornby <hello@joshhornby.co.uk>
2014-07-14 21:23:12 +0100

    Fixes

--- src/Sly/NotificationPusher/Adapter/Gcm.php
+++ src/Sly/NotificationPusher/Adapter/Gcm.php
@@ -88,10 +88,11 @@ class Gcm extends BaseAdapter
     {
         $client->setApiKey($this->getParameter('apiKey'));
 
-        $new_client = new \Zend\Http\Client(null, array(
+        $newClient = new \Zend\Http\Client(null, array(
             'adapter'       => 'Zend\Http\Client\Adapter\Socket',
             'sslverifypeer' => false
         ));
+        
         $client->setHttpClient($newClient);
 
         return $client;
