8e277b6b291a289dfdb7248b73d43ff5e06d2f8b
Thijs Scheepers <tscheepers@users.noreply.github.com>
2016-03-01 20:34:24 +0100

    Added urlArgs for Safari push notifications

--- src/Sly/NotificationPusher/Adapter/Apns.php
+++ src/Sly/NotificationPusher/Adapter/Apns.php
@@ -168,6 +168,7 @@ class Apns extends BaseAdapter
         $sound = $message->getOption('sound', 'bingbong.aiff');
         $contentAvailable = $message->getOption('content-available');
         $category = $message->getOption('category');
+        $urlArgs = $message->getOption('urlArgs');
 
         $alert = new ServiceAlert(
             $message->getText(),
@@ -220,6 +221,10 @@ class Apns extends BaseAdapter
 
         if (null !== $category) {
             $serviceMessage->setCategory($category);
+        }
+
+        if (null !== $urlArgs) {
+            $serviceMessage->setUrlArgs($urlArgs);
         }
 
         return $serviceMessage;
